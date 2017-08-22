89e37578616e12164bb7620ca3f855f4e51b41c4
gpanah <gpanah1927@gmail.com>
2017-08-22 12:47:51 -0500

    update

---
+++ javascript/time.js
@@ -0,0 +1,7 @@
+const dateTime = "2017-08-18T16:12:00.0000000Z"
+const anotherDateTime = "2016-08-18T16:12:00.0000000Z"
+
+const aDate = new Date(dateTime)
+const anotherDate = new Date(anotherDateTime)
+
+aDate.setFullYear(1995)
